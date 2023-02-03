065faeb85d25e8e83b4c39d9e7b6d01d960763f5
chuheeseung <youverygood@naver.com>
2023-02-03 15:36:21 +0900

    23.02.03

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
 		console.log('app js');
 	}, []);
 	return (
-		<BrowserRouter basename={process.env.REACT_APP_PUBLIC_URL}>
+		<BrowserRouter basename={process.env.PUBLIC_URL}>
 			<Header />
 			<Routes>
 				<Route path="/" element={<TodoPage />} />
